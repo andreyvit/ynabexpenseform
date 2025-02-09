3e6c0734afcfa657f5f931b3e7fd5b28923eba7c
Andrey Tarantsov <andrey@tarantsov.com>
2025-02-09 22:32:02 +0400

    Restart on deploy

--- deploy-remote.sh
+++ deploy-remote.sh
@@ -59,5 +59,5 @@ EOF
 
 SUDO systemctl daemon-reload
 SUDO systemctl enable ynabexpenseform
-SUDO systemctl start ynabexpenseform
+SUDO systemctl restart ynabexpenseform
 /srv/caddy/bin/caddy reload --config /etc/Caddyfile
